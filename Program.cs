1752321f182e981dcd3cafa4024e5252698bd5b3
Moloko Sehata <moloko4@live.com>
2023-10-09 10:40:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Drawing_Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("              /|");
+            Console.WriteLine("             / |");
+            Console.WriteLine("            /  |");
+            Console.WriteLine("           /   |");
+            Console.WriteLine("          /    |");
+            Console.WriteLine("         /     |");
+            Console.WriteLine("        /      |");
+            Console.WriteLine("        -------");
+            Console.ReadLine();
+        }
+    }
+}
